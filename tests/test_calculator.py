4582c91c51d038d5b8d14b31109aff870bf267bb
DEV-COM <kwp@tlen.pl>
2024-08-03 16:18:31 +0200

    nowy workflow, cz15

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,5 +1,4 @@
 import unittest
-
 from calculator import Calculator
 
 
@@ -10,15 +9,11 @@ class TestCalculator(unittest.TestCase):
         result = calculator.add(10, 20)
         self.assertEqual(result, 30)
 
-
     def test_multiply(self):
         calculator = Calculator()
         result = calculator.multiply(2, 2)
         self.assertEqual(result, 4)
 
 
-
 if __name__ == '__main__':
     unittest.main()
-
-

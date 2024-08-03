80484dbbd7ed6300c5fe3818908b2ced9e6d3590
DEV-COM <kwp@tlen.pl>
2024-08-03 10:36:39 +0200

    nowe testy

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,5 +1,7 @@
 import unittest
+
 from calculator import Calculator
+
 
 class TestCalculator(unittest.TestCase):
 
@@ -8,5 +10,15 @@ class TestCalculator(unittest.TestCase):
         result = calculator.add(10, 20)
         self.assertEqual(result, 30)
 
+
+    def test_multiply(self):
+        calculator = Calculator()
+        result = calculator.multiply(2, 2)
+        self.assertEqual(result, 4)
+
+
+
 if __name__ == '__main__':
     unittest.main()
+
+
